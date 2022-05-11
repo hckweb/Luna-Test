3e4ef69ca0139a4c81ec60e0d7923039a19547ee
Cem KOÇ <koc.huseyincem@gmail.com>
2022-05-12 00:53:03 +0300

    change uint8_t pointer to vector in sdio class

--- source/lib/peripheral/sdio.h
+++ source/lib/peripheral/sdio.h
@@ -5,6 +5,7 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <vector>
 
 enum class Sdio_mode {
     HOST,
@@ -16,9 +17,9 @@ public:
     Sdio(GPIO_no cmd, GPIO_no clk, GPIO_no data0, GPIO_no data1, GPIO_no data2, GPIO_no data3, Sdio_mode mode);
     Sdio(Sdio&) = delete; // copy constructor is deleted
 
-    uint8_t* read_buffer();
+    std::vector<char> read_buffer();
 
-    void write_byte(uint8_t* buffer, size_t buffer_size);
+    void write_buffer(std::vector<char> data);
 
 private:
     GPIO_no _cmd;
