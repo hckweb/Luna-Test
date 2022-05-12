835ec315358b88d963aa50a8bcca6ac4bafbfcf5
Cem KOÇ <koc.huseyincem@gmail.com>
2022-05-12 21:32:32 +0300

    add wifi class

---
+++ source/lib/onboard_devices/wifi.h
@@ -0,0 +1,44 @@
+#ifndef WIFI_H
+#define WIFI_H
+
+#include "../peripheral/network_common.h"
+#include "../peripheral/uart.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+enum class EncryptionProtocol {
+    NONE,
+    WEP,
+    WPA,
+    WPA2,
+    AUTO,
+};
+
+class Wifi {
+public:
+    Wifi(GPIO_no tx, GPIO_no rx)
+    {
+        _uart = std::make_unique<Uart>(tx, rx, 115200);
+        // something more
+    }
+    Wifi(const Wifi&) = delete; // copy constructor is deleted
+
+    void connect(std::string ssid, std::string pass, EncryptionProtocol protocol = EncryptionProtocol::AUTO);
+    void disconnect();
+
+    bool is_connected();
+    ipv4_t get_ipv4();
+    ipv6_t get_ipv6();
+
+private:
+    std::unique_ptr<Uart> _uart;
+    std::unique_ptr<std::vector<TCP>> _tcps;
+    std::unique_ptr<std::vector<UDP>> _udps;
+    std::unique_ptr<std::vector<HTTPRequest>> _https;
+    ipv4_t _ipv4;
+    ipv6_t _ipv6;
+};
+
+#endif
